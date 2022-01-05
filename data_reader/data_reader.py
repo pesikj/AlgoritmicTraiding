88c9cc730ac6f6ba52ae417e5befc9097ebb4f5d
Jiří Pešík <jiri.pesik@gmail.com>
2022-01-05 22:07:01 +0100

    Initial button panel #5

--- data_reader/data_reader.py
+++ data_reader/data_reader.py
@@ -9,19 +9,21 @@ DATA_FOLDER = "data"
 class DataReader:
     api_key: str
 
-    def _send_data_request(self, symbol, start_date):
+    def __init__(self):
+        self.api_key = os.getenv("API_KEY")
+
+    def _send_data_request(self, symbol):
         # api_key = os.getenv("API_KEY")
         api_url = f"https://api.twelvedata.com/time_series" \
                   f"?symbol={symbol}&interval=1day&outputsize=5000&apikey={self.api_key}"
         raw_df = requests.get(api_url).json()
         df = pd.DataFrame(raw_df['values']).iloc[::-1].set_index('datetime').astype(float)
-        df = df[df.index >= start_date]
         df.index = pd.to_datetime(df.index)
         df.to_csv(f"data/{symbol}.csv")
 
     def get_dataframe(self, symbol):
         path = os.path.join(DATA_FOLDER, f"{symbol}.csv")
         if not os.path.isfile(path):
-            self._send_data_request()
+            self._send_data_request(symbol)
         df = pd.read_csv(path)
         return df
--- gui/app.py
+++ gui/app.py
@@ -6,34 +6,65 @@ from matplotlib.figure import Figure
 from charter.charter import Charter
 from data_reader.data_reader import DataReader
 from stock_analyzer import stock_analyzer
+import tkcalendar
 
 
 class ChartFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.pack()
+        self.initUI()
+        # self.pack()
+        #
+        # self.entrythingy = tk.Entry()
+        # self.entrythingy.pack()
+        #
+        # self.contents = tk.StringVar()
+        # self.contents.set("GOOGL")
+        # self.entrythingy["textvariable"] = self.contents
+        #
+        # # Define a callback for when the user hits return.
+        # # It prints the current value of the variable.
+        # self.entrythingy.bind('<Key-Return>', self.print_contents)
+        #
+        # self.btn_add_chart = tk.Button(master=self.master, text="Load stock", command=self.add_chart)
+        # self.btn_add_chart.pack()
+        #
+        # self.fig = Figure(figsize=(5, 4), dpi=100)
+        # self.ax = self.fig.add_subplot(111)
+        #
+        # self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
+        # self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-        self.entrythingy = tk.Entry()
-        self.entrythingy.pack()
+        self.data_reader = DataReader()
 
-        self.contents = tk.StringVar()
-        self.contents.set("GOOGL")
-        self.entrythingy["textvariable"] = self.contents
-
-        # Define a callback for when the user hits return.
-        # It prints the current value of the variable.
-        self.entrythingy.bind('<Key-Return>', self.print_contents)
-
-        self.btn_add_chart = tk.Button(master=self.master, text="Load stock", command=self.add_chart)
-        self.btn_add_chart.pack()
+    def initUI(self):
+        plot_frame = tk.Frame(self.master)
+        plot_frame.pack(fill=tk.BOTH)
 
         self.fig = Figure(figsize=(5, 4), dpi=100)
         self.ax = self.fig.add_subplot(111)
-
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=plot_frame)
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-        self.data_reader = DataReader()
+        button_frame = tk.Frame(self.master)
+
+        lbl1 = tk.Label(button_frame, text="Symbol").grid(row=0, sticky=tk.W)
+        self.entry_symbol = tk.Entry(button_frame)
+        self.entry_symbol.grid(row=0, column=1)
+
+        self.contents = tk.StringVar()
+        self.contents.set("GOOGL")
+        self.entry_symbol["textvariable"] = self.contents
+
+        lbl2 = tk.Label(button_frame, text="Date from").grid(row=1, sticky=tk.W)
+        self.entry_date_from = tk.Entry(button_frame)
+        self.entry_date_from.grid(row=1, column=1)
+
+        btn_add_chart = tk.Button(master=button_frame, text="Load stock", command=self.add_chart)
+        btn_add_chart.grid(row=2, sticky=tk.W)
+
+        button_frame.pack(fill=tk.BOTH)
+
 
     def print_contents(self, event):
         print("Hi. The current entry content is:",
